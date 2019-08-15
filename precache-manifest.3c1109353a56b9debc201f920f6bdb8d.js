268bcf8f33c66528154ab781eb81cf278a807b72
Sneha <bhartisneha101@gmail.com>
2019-08-15 23:23:00 +0530

    Updates

---
+++ precache-manifest.3c1109353a56b9debc201f920f6bdb8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c53423ec166696a8f2518adf1ffead9",
+    "url": "/chatWindow/index.html"
+  },
+  {
+    "revision": "1f97a0a8d44949ec0aff",
+    "url": "/chatWindow/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "c14bcd5de937bd687708",
+    "url": "/chatWindow/static/css/main.6b2be22e.chunk.css"
+  },
+  {
+    "revision": "1f97a0a8d44949ec0aff",
+    "url": "/chatWindow/static/js/2.6db49d3f.chunk.js"
+  },
+  {
+    "revision": "c14bcd5de937bd687708",
+    "url": "/chatWindow/static/js/main.59bea554.chunk.js"
+  },
+  {
+    "revision": "7062646b937059440c92",
+    "url": "/chatWindow/static/js/runtime~main.8e1952b7.js"
+  }
+]);
